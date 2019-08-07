629806674db93a6b977c00e9d2805b531a7dbc72
awalsetya <51275077+awalsetya@users.noreply.github.com>
2019-08-08 04:28:25 +0700

    hari 3

---
+++ novice/01-03/classbuah.dart
@@ -0,0 +1,8 @@
+//field "variabel yang di deklarasikan di kelas"
+class Buah {
+  String namabuah ="Anggur";
+//function "melakukan tindakan yang dapat dilakukan suatu objek / disebut juga method" 
+  void Buahku(){
+    print(namabuah);
+  }
+}
