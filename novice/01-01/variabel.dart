ff1948b7a7c4299579d0c2ae8889c2b735483933
awalsetya <51275077+awalsetya@users.noreply.github.com>
2019-08-07 22:04:46 +0700

    hari 1

---
+++ novice/01-01/variabel.dart
@@ -0,0 +1,14 @@
+void main(){
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+    //mencetak variabel
+    print("Nama  $name. Umur $year tahun. lingkar badan $antennaDiameter cm. ");
+    print("Planet : $flybyObjects");
+    print("Gambar: $image");
+}
