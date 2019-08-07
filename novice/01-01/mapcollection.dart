75cf26a0cbb39f84cde0a0fe20eee6a13b7d18ea
awalsetya <51275077+awalsetya@users.noreply.github.com>
2019-08-07 22:04:15 +0700

    hari 1

---
+++ novice/01-01/mapcollection.dart
@@ -0,0 +1,13 @@
+main(){
+    Map pasien = {
+        "nama" :{
+            "depan": "Aw",
+            "belakang" : "Wal"
+        },
+        "usia" :67,
+        "alamat" : "SLeman"
+    };
+
+    print(pasien['nama']['depan']+ ''+ pasien['nama']['belakang']);
+    print(pasien['nama']);
+}
