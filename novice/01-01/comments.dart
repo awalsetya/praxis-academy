c7dbe530c9b5c6a4127f0f9ccde98f09bd748036
awalsetya <51275077+awalsetya@users.noreply.github.com>
2019-08-07 22:01:37 +0700

    hari 1

---
+++ novice/01-01/comments.dart
@@ -0,0 +1,5 @@
+/ This is a normal, one-line comment.
+
+/// This is a documentation comment, used to document libraries,
+/// classes, and their members. Tools like IDEs and dartdoc treat
+/// doc comments specially.
