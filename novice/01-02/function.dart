128f5cd8150db52564e1bcb0c6d710a03dbd28c6
awalsetya <51275077+awalsetya@users.noreply.github.com>
2019-08-07 22:07:10 +0700

    hari 2

---
+++ novice/01-02/function.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+//membuat fungsi 
+angkaku(int angka){
+  print ('ini angkaku $angka.');
+}
+
+//eksekusi program
+main(){
+  var angka = 42; // mendeklarasikan variabel
+  angkaku(angka);
+}
